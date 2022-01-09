e7a41ae2e7f0fccc49913d0036cf8ac59f54b77f
Michel Christophe <mchristopheweb@gmail.com>
2022-01-09 13:45:12 +0100

    ajout securité pour la page confirmation

--- js/class/Order.js
+++ js/class/Order.js
@@ -9,7 +9,7 @@ class Order{
     constructor(){
 
         this.orderIdElement = document.getElementById("orderId");
-
+        this.regexOrder = /^[a-zA-Z0-9]{8}(-[a-zA-Z0-9]{4}){3}-[a-zA-Z0-9]{12}$/
         this.sendOrderId();
     }
 
@@ -18,10 +18,23 @@ class Order{
      * get the orderId into the url page and insert into the DOM
      */
     sendOrderId(){
+        
+        try {
+            let url = location.href;
+            let orderId = getParam(url, 'orderId');  
 
-        let url = location.href;
-        let orderId = getParam(url, 'orderId');
-        this.orderIdElement.innerHTML = orderId;
+            if (!orderId || !this.regexOrder.test(orderId)){
+                throw new Error();
+            }    
+
+            this.orderIdElement.innerHTML = orderId;
+            
+        } catch (err){
+            
+            location.href="./index.html";
+        }
+        
+        
         
     }
 
